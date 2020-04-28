3a55355749832de71b7d2778856ddb111c1dd267
Caroline Hoeg <carolinehoeg123@gmail.com>
2020-04-28 09:49:49 +0200

    MovieDTO

---
+++ src/main/java/dto/MovieDTO.java
@@ -0,0 +1,77 @@
+package dto;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author carol
+ */
+public class MovieDTO {
+    
+    private final String title;
+    private final String releaseYear;
+    private final String poster;
+    private final String imdbId;
+
+    public MovieDTO(String title, String releaseYear, String poster, String imdbId) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.poster = poster;
+        this.imdbId = imdbId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + Objects.hashCode(this.title);
+        hash = 23 * hash + Objects.hashCode(this.releaseYear);
+        hash = 23 * hash + Objects.hashCode(this.poster);
+        hash = 23 * hash + Objects.hashCode(this.imdbId);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MovieDTO other = (MovieDTO) obj;
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.releaseYear, other.releaseYear)) {
+            return false;
+        }
+        if (!Objects.equals(this.poster, other.poster)) {
+            return false;
+        }
+        if (!Objects.equals(this.imdbId, other.imdbId)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
