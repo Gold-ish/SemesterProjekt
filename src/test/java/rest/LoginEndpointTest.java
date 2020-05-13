f5331119d1a3699f54e0984e4621217a252cc97e
789Michael <allanalli@hotmail.com>
2020-05-13 15:19:49 +0200

    fixed test

--- src/test/java/rest/LoginEndpointTest.java
+++ src/test/java/rest/LoginEndpointTest.java
@@ -93,7 +93,6 @@ public class LoginEndpointTest {
     }
 
     //This is how we hold on to the token after login, similar to that a client must store the token somewhere
-<<<<<<< HEAD
     private static String securityToken;
 
     //Utility method to login and set the returned securityToken
@@ -191,160 +190,11 @@ public class LoginEndpointTest {
 
     @Test
     public void testRestForMultiRole2() {
-=======
-  private static String securityToken;
-
-  //Utility method to login and set the returned securityToken
-  private static void login(String role, String password) {
-    String json = String.format("{username: \"%s\", password: \"%s\"}", role, password);
-    securityToken = given()
-            .contentType("application/json")
-            .body(json)
-            //.when().post("/api/login")
-            .when().post("/login")
-            .then()
-            .extract().path("token");
-      System.out.println("TOKEN ---> "+securityToken);
-  }
-
-  private void logOut() {
-    securityToken = null;
-  }
-
-  @Test
-  public void serverIsRunning() {
-    System.out.println("Testing is server UP");
-    given().when().get("/info").then().statusCode(200);
-  }
-
-  @Test
-  public void testRestNoAuthenticationRequired() {
-    given()
-            .contentType("application/json")
-            .when()
-            .get("/info").then()
-            .statusCode(200)
-            .body("msg", equalTo("Hello anonymous"));
-  }
-
-  @Test
-  public void testRestForAdmin() {
-    login("admin", "test");
-    given()
-            .contentType("application/json")
-            .accept(ContentType.JSON)
-            .header("x-access-token", securityToken)
-            .when()
-            .get("/info/admin").then()
-            .statusCode(200)
-            .body("msg", equalTo("Hello to (admin) User: admin"));
-  }
-
-  @Test
-  public void testRestForUser() {
-    login("user", "test");
-    given()
-            .contentType("application/json")
-            .header("x-access-token", securityToken)
-            .when()
-            .get("/info/user").then()
-            .statusCode(200)
-            .body("username", equalTo("user"));
-  }
-  
-  @Test
-  public void testRestForCritic() {
-    login("critic", "test");
-    given()
-            .contentType("application/json")
-            .header("x-access-token", securityToken)
-            .when()
-            .get("/info/user").then()
-            .statusCode(200)
-            .body("username", equalTo("critic"));
-  }
-  
-  @Test
-  public void testAutorizedUserCannotAccesAdminPage() {
-    login("user", "test");
-    given()
-            .contentType("application/json")
-            .header("x-access-token", securityToken)
-            .when()
-            .get("/info/admin").then()  //Call Admin endpoint as user
-            .statusCode(401);
-  }
-  
-  @Test
-  public void testAutorizedAdminCannotAccesUserPage() {
-    login("admin", "test");
-    given()
-            .contentType("application/json")
-            .header("x-access-token", securityToken)
-            .when()
-            .get("/info/user").then()  //Call User endpoint as Admin
-            .statusCode(401);
-  }
-  
-  @Test
-  public void testRestForMultiRole1() {
-    login("user_admin", "test");
-    given()
-            .contentType("application/json")
-            .accept(ContentType.JSON)
-            .header("x-access-token", securityToken)
-            .when()
-            .get("/info/admin").then()
-            .statusCode(200)
-            .body("msg", equalTo("Hello to (admin) User: user_admin"));
-  }
-
-  @Test
-  public void testRestForMultiRole2() {
-    login("user_admin", "test");
-    given()
-            .contentType("application/json")
-            .header("x-access-token", securityToken)
-            .when()
-            .get("/info/user").then()
-            .statusCode(200)
-            .body("username", equalTo("user_admin"));
-  }
-
-  @Test
-  public void userNotAuthenticated() {
-    logOut();
-    given()
-            .contentType("application/json")
-            .when()
-            .get("/info/user").then()
-            .statusCode(403)
-            .body("code", equalTo(403))
-            .body("message", equalTo("Not authenticated - do login"));
-  }
-
-  @Test
-  public void adminNotAuthenticated() {
-    logOut();
-    given()
-            .contentType("application/json")
-            .when()
-            .get("/info/admin").then()
-            .statusCode(403)
-            .body("code", equalTo(403))
-            .body("message", equalTo("Not authenticated - do login"));
-  }
-    
-    
-    @Test
-    public void testGetCriticCode_ReturnsRandomGeneratedString_EqualResults() {
->>>>>>> criticbranch
         login("user_admin", "test");
         given()
                 .contentType("application/json")
                 .header("x-access-token", securityToken)
                 .when()
-<<<<<<< HEAD
                 .get("/info/user").then()
                 .statusCode(200)
                 .body("username", equalTo("user_admin"));
@@ -361,7 +211,19 @@ public class LoginEndpointTest {
                 .body("code", equalTo(403))
                 .body("message", equalTo("Not authenticated - do login"));
     }
-
+    
+    
+     @Test
+  public void testRestForCritic() {
+    login("critic", "test");
+    given()
+            .contentType("application/json")
+            .header("x-access-token", securityToken)
+            .when()
+            .get("/info/user").then()
+            .statusCode(200)
+            .body("username", equalTo("critic"));
+  }
     @Test
     public void adminNotAuthenticated() {
         logOut();
@@ -372,10 +234,7 @@ public class LoginEndpointTest {
                 .statusCode(403)
                 .body("code", equalTo(403))
                 .body("message", equalTo("Not authenticated - do login"));
-=======
-                .get("/info/critic/code").then()
-                .statusCode(200);
->>>>>>> criticbranch
+
     }
 
 }
